178032c80e78e403eae1bbb696e197dff266e24a
AdamDonovan <adonovan@riskalyze.com>
2018-07-10 17:28:19 -0700

    Create cards.js

---
+++ cards/cards.js
@@ -0,0 +1,452 @@
+[
+  {
+    "NAME": "Embodiment of Being Tired",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 30,
+    "DEFENSE": 48,
+    "SPEED": 22,
+    "TYPE": "Rock",
+    "BASE STAT": 100,
+    "STATED PRIORITY": "High"
+  },
+  {
+    "NAME": "Dork Ork",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 33,
+    "DEFENSE": 30,
+    "SPEED": 37,
+    "TYPE": "Paper",
+    "BASE STAT": 100,
+    "STATED PRIORITY": "High"
+  },
+  {
+    "NAME": "Nick Cage’s Wife",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 21,
+    "DEFENSE": 22,
+    "SPEED": 57,
+    "TYPE": "Scissors",
+    "BASE STAT": 100,
+    "STATED PRIORITY": "High"
+  },
+  {
+    "NAME": "Fake Flower",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 20,
+    "DEFENSE": 60,
+    "SPEED": 20,
+    "TYPE": "Scissors",
+    "BASE STAT": 100,
+    "STATED PRIORITY": "High"
+  },
+  {
+    "NAME": "Bad Tipper",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 27,
+    "DEFENSE": 30,
+    "SPEED": 43,
+    "TYPE": "Scissors",
+    "BASE STAT": 100,
+    "STATED PRIORITY": "High"
+  },
+  {
+    "NAME": "Printer",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 23,
+    "DEFENSE": 47,
+    "SPEED": 20,
+    "TYPE": "Rock",
+    "BASE STAT": 100,
+    "STATED PRIORITY": "High"
+  },
+  {
+    "NAME": "Road Boat",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 25,
+    "DEFENSE": 39,
+    "SPEED": 61,
+    "TYPE": "Rock",
+    "BASE STAT": 125,
+    "STATED PRIORITY": "High"
+  },
+  {
+    "NAME": "Spork Ork",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 35,
+    "DEFENSE": 60,
+    "SPEED": 30,
+    "TYPE": "Scissors",
+    "BASE STAT": 125,
+    "STATED PRIORITY": "High"
+  },
+  {
+    "NAME": "Balloon with a Blowgun",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 60,
+    "DEFENSE": 20,
+    "SPEED": 45,
+    "TYPE": "Paper",
+    "BASE STAT": 125,
+    "STATED PRIORITY": "High"
+  },
+  {
+    "NAME": "Mean Bird",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 33,
+    "DEFENSE": 29,
+    "SPEED": 63,
+    "TYPE": "Scissors",
+    "BASE STAT": 125,
+    "STATED PRIORITY": "High"
+  },
+  {
+    "NAME": "Octo Snake",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 50,
+    "DEFENSE": 20,
+    "SPEED": 55,
+    "TYPE": "Rock",
+    "BASE STAT": 125,
+    "STATED PRIORITY": "High"
+  },
+  {
+    "NAME": "Fork Ork",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 38,
+    "DEFENSE": 58,
+    "SPEED": 34,
+    "TYPE": "Rock",
+    "BASE STAT": 130,
+    "STATED PRIORITY": "High"
+  },
+  {
+    "NAME": "Unlucky Homeless Man",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 60,
+    "DEFENSE": 39,
+    "SPEED": 31,
+    "TYPE": "Rock",
+    "BASE STAT": 130,
+    "STATED PRIORITY": "High"
+  },
+  {
+    "NAME": "Stork Ork",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 80,
+    "DEFENSE": 55,
+    "SPEED": 35,
+    "TYPE": "Paper",
+    "BASE STAT": 170,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Hornless Unicorn",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 45,
+    "DEFENSE": 52,
+    "SPEED": 73,
+    "TYPE": "Paper",
+    "BASE STAT": 170,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Lincoln’s Ghost",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 20,
+    "DEFENSE": 110,
+    "SPEED": 20,
+    "TYPE": "Scissors",
+    "BASE STAT": 150,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Too Many Tattoos",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 38,
+    "DEFENSE": 53,
+    "SPEED": 59,
+    "TYPE": "Scissors",
+    "BASE STAT": 150,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Thoughtful Whale",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 37,
+    "DEFENSE": 39,
+    "SPEED": 74,
+    "TYPE": "Scissors",
+    "BASE STAT": 150,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Bork Ork",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 65,
+    "DEFENSE": 30,
+    "SPEED": 55,
+    "TYPE": "Paper",
+    "BASE STAT": 150,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Small Child",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 35,
+    "DEFENSE": 57,
+    "SPEED": 58,
+    "TYPE": "Paper",
+    "BASE STAT": 150,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Hairy Elf",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 36,
+    "DEFENSE": 38,
+    "SPEED": 76,
+    "TYPE": "Scissors",
+    "BASE STAT": 150,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Blenderman",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 55,
+    "DEFENSE": 30,
+    "SPEED": 65,
+    "TYPE": "Paper",
+    "BASE STAT": 150,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Judge Bread",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 86,
+    "DEFENSE": 66,
+    "SPEED": 23,
+    "TYPE": "Scissors",
+    "BASE STAT": 175,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Sexy Female Character",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 85,
+    "DEFENSE": 20,
+    "SPEED": 70,
+    "TYPE": "Paper",
+    "BASE STAT": 175,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Pork Ork",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 62,
+    "DEFENSE": 64,
+    "SPEED": 49,
+    "TYPE": "Scissors",
+    "BASE STAT": 175,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Furry Robot",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 47,
+    "DEFENSE": 84,
+    "SPEED": 44,
+    "TYPE": "Scissors",
+    "BASE STAT": 175,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Mass Unemployment",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 84,
+    "DEFENSE": 70,
+    "SPEED": 31,
+    "TYPE": "Scissors",
+    "BASE STAT": 185,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Bad Wizard",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 70,
+    "DEFENSE": 43,
+    "SPEED": 72,
+    "TYPE": "Rock",
+    "BASE STAT": 185,
+    "STATED PRIORITY": "Medium"
+  },
+  {
+    "NAME": "Clown with Hot Coffee",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 50,
+    "DEFENSE": 48,
+    "SPEED": 117,
+    "TYPE": "Paper",
+    "BASE STAT": 215,
+    "STATED PRIORITY": "Low"
+  },
+  {
+    "NAME": "Mertaur (half mermaid, half centaur)",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 76,
+    "DEFENSE": 75,
+    "SPEED": 65,
+    "TYPE": "Paper",
+    "BASE STAT": 215,
+    "STATED PRIORITY": "Low"
+  },
+  {
+    "NAME": "Bjork Ork",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 63,
+    "DEFENSE": 89,
+    "SPEED": 43,
+    "TYPE": "Paper",
+    "BASE STAT": 195,
+    "STATED PRIORITY": "Low"
+  },
+  {
+    "NAME": "Uncork Ork",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 66,
+    "DEFENSE": 84,
+    "SPEED": 45,
+    "TYPE": "Paper",
+    "BASE STAT": 195,
+    "STATED PRIORITY": "Low"
+  },
+  {
+    "NAME": "Pitchfork Ork",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 90,
+    "DEFENSE": 77,
+    "SPEED": 33,
+    "TYPE": "Paper",
+    "BASE STAT": 200,
+    "STATED PRIORITY": "Low"
+  },
+  {
+    "NAME": "Chem trails Airplane",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 53,
+    "DEFENSE": 99,
+    "SPEED": 48,
+    "TYPE": "Rock",
+    "BASE STAT": 200,
+    "STATED PRIORITY": "Low"
+  },
+  {
+    "NAME": "HateMan",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 70,
+    "DEFENSE": 70,
+    "SPEED": 60,
+    "TYPE": "Scissors",
+    "BASE STAT": 200,
+    "STATED PRIORITY": "Low"
+  },
+  {
+    "NAME": "Political Tooth",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 50,
+    "DEFENSE": 130,
+    "SPEED": 20,
+    "TYPE": "Rock",
+    "BASE STAT": 200,
+    "STATED PRIORITY": "Low"
+  },
+  {
+    "NAME": "Germ",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 94,
+    "DEFENSE": 103,
+    "SPEED": 23,
+    "TYPE": "Scissors",
+    "BASE STAT": 220,
+    "STATED PRIORITY": "Low"
+  },
+  {
+    "NAME": "New York Ork",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 60,
+    "DEFENSE": 125,
+    "SPEED": 35,
+    "TYPE": "Scissors",
+    "BASE STAT": 220,
+    "STATED PRIORITY": "Low"
+  },
+  {
+    "NAME": "Two Kids Standing on Each Other's Shoulders Wearing a Trenchcoat",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 104,
+    "DEFENSE": 51,
+    "SPEED": 65,
+    "TYPE": "Paper",
+    "BASE STAT": 220,
+    "STATED PRIORITY": "Low"
+  },
+  {
+    "NAME": "Tire Fire Ted",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 75,
+    "DEFENSE": 58,
+    "SPEED": 87,
+    "TYPE": "Paper",
+    "BASE STAT": 220,
+    "STATED PRIORITY": "Low"
+  },
+  {
+    "NAME": "Muscular Brain",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 119,
+    "DEFENSE": 99,
+    "SPEED": 32,
+    "TYPE": "Paper",
+    "BASE STAT": 250,
+    "STATED PRIORITY": "Very Low"
+  },
+  {
+    "NAME": "Seasonal Allegies",
+    "Picture": "https://raw.githubusercontent.com/AdamDonovan/gametest/master/images/woman_avatar.png",
+    "ATTACK": 120,
+    "DEFENSE": 83,
+    "SPEED": 57,
+    "TYPE": "Paper",
+    "BASE STAT": 260,
+    "STATED PRIORITY": "Very Low"
+  },
+  {
+    "NAME": "Y2K",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 60,
+    "DEFENSE": 55,
+    "SPEED": 155,
+    "TYPE": "Rock",
+    "BASE STAT": 270,
+    "STATED PRIORITY": "Very Low"
+  },
+  {
+    "NAME": "8 Bit CEO",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 145,
+    "DEFENSE": 115,
+    "SPEED": 30,
+    "TYPE": "Paper",
+    "BASE STAT": 290,
+    "STATED PRIORITY": "Extremely Low"
+  },
+  {
+    "NAME": "The Moon with a Gun",
+    "Picture": "https://github.com/AdamDonovan/gametest/blob/master/images/man_avatar.png?raw=true",
+    "ATTACK": 110,
+    "DEFENSE": 100,
+    "SPEED": 110,
+    "TYPE": "Paper",
+    "BASE STAT": 320,
+    "STATED PRIORITY": "Extremely Low"
+  }
+]
